eb5341d7600840dc219de2c9409c748cead41995
David Melinosky <dmelinosky@gmail.com>
2020-05-10 16:27:59 -0400

    Add project files.

---
+++ MinAPI/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace MinAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configuration.MapHttpAttributeRoutes();
+
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var jsonFormatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
+            GlobalConfiguration.Configuration.EnsureInitialized();
+        }
+    }
+}
